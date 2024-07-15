96533b91b4000cf3aca64441b0b158f76001d0f2
James Ives <iam@jamesiv.es>
2024-07-15 08:34:31 -0400

    Update entrypoint.sh

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -7,6 +7,7 @@ if echo "$@" | grep -qv "+map"; then
   echo -e "\e[33mWarning: No +map specified in the command. Server will start but may not be joinable.\e[0m"
 fi
 
+# Push mods and config files from their temp directories to the server directories.
 if [ -d /temp/mods ]
 then
   rsync --chown=steam:steam /temp/mods/* /opt/steam/hlds
@@ -19,4 +20,5 @@ fi
 
 echo -e "\e[32mStarting Half-Life Dedicated Server for $GAME...\e[0m"
 
+# Start the server with the specified game and any additional arguments.
 ./hlds_run "-game $GAME $@"
