6d010a12b3ec58cb09b7cef2c884346e1e28f10a
James Ives <iam@jamesiv.es>
2024-09-07 10:07:22 -0400

    build(startup): :wrench: Setup initial build debug scripts

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
-# Set GAME to its value if defined, or 'valve' if not
 GAME=${GAME:-valve}
+VERSION=${VERSION:-custom}
 
 if echo "$@" | grep -qv "+map"; then
   echo -e "\e[33mWarning: No +map specified in the command. Server will start but may not be joinable.\e[0m"
@@ -19,6 +19,53 @@ then
 fi
 
 echo -e "\e[32mStarting Half-Life Dedicated Server for $GAME...\e[0m"
+
+
+IRed='\033[0;91m'         # Red
+IYellow='\033[0;93m'      # Yellow
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+Color_Off='\033[0m' 
+
+echo -e "\033[0;91m
+                          ..::::::..              
+                      :-=++++++++++++=-:          
+                  :=++++=--::...::-=++++=:       
+                :=+++=:              :-++++:     
+                =+++-     =====:         -+++=    
+              ++++.      ===+++.         .=+++   
+              =+++           :+++           =+++  
+            :+++.           -+++=          .+++: 
+            =++=           =+++++-          =++= 
+            =++-         .=++-:+++:         -+++ 
+            =++=        .+++-  -+++.        =++= 
+            :+++.      :+++.    =+++       .+++: 
+              =+++     =++=.      ++++++=   =++=  
+              =+++.  -==-        .+++=-: .=+++   
+                =+++-.                   -+++=    
+                :=+++=:              :=+++=:     
+                  :=+++++=-::..::-=+++++=:       
+                      :-=++++++++++++=-:          
+                          ..::::::..              
+\033[0;97m
+                          hlds-docker 
+${IYellow}
+▄▄ ${IWhite}LINKS${IYellow} ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+█                                                                  █
+█  ${IWhite}🔧 Maintained by Jives: https://jives.dev${IYellow}                       █
+█  ${IWhite}💖 Support: https://github.com/sponsors/JamesIves${IYellow}               █
+█  ${IWhite}🔔 Feedback / Issues: https://github.com/JamesIves/hlds-docker${IYellow}  █
+█                                                                  █
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+${IPurple}
+▄▄ ${IWhite}STARTUP${IPurple} ▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄
+█                                                                  █
+█  ${IWhite}🔧 Container Version: ${VERSION} ${IPurple}                                   █
+█                                                                  █
+▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀
+\033[0m
+"
 
 # Start the server with the specified game and any additional arguments.
 ./hlds_run "-game $GAME $@"
