104de8161b333fb3a864acd1930cf5544f79ff72
James Ives <iam@jamesiv.es>
2024-10-03 09:21:10 -0400

    debug

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,26 +1,33 @@
 #!/usr/bin/env sh
-
 GAME=${GAME:-valve}
 VERSION=${VERSION:-custom}
 IMAGE=${IMAGE:-custom}
 
-# Adds a delay to ensure volumes are mounted properly
-sleep 10
-
 if echo "$@" | grep -qv "+map"; then
   echo -e "\e[33mWarning: No +map specified in the command. Server will start but may not be joinable.\e[0m"
 fi
+
+# Debug: List contents of temp directories
+echo "Listing contents of /temp/mods:"
+ls -l /temp/mods
+echo "Listing contents of /temp/config:"
+ls -l /temp/config
 
 # Push mods and config files from their temp directories to the server directories.
 if [ -d /temp/mods ]
 then
   rsync --recursive --update --chown=steam:steam /temp/mods/* /opt/steam/hlds
 fi
-
 if [ -d /temp/config ]
 then
   rsync --recursive --update --chown=steam:steam /temp/config/* /opt/steam/hlds/$GAME
 fi
+
+# Debug: List contents of target directories after rsync
+echo "Listing contents of /opt/steam/hlds:"
+ls -l /opt/steam/hlds
+echo "Listing contents of /opt/steam/hlds/$GAME:"
+ls -l /opt/steam/hlds/$GAME
 
 
 echo "
